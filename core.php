2a17d19028a6a2e30032f56b3702ecff72bbf803
Volkan Şah //NCF <VolkanSah@users.noreply.github.com>
2023-06-23 13:00:48 +0200

    Update core.php

--- core.php
+++ core.php
@@ -16,11 +16,15 @@
  */
 function content_warning_filter($content) {
     global $post;
-    if (has_category('was-wuerde-onkel-volkan-davon-halten', $post->ID)) {
-        $content = '<p>Der folgende Beitrag ist möglicherweise nicht für Kinder und Jugendliche unter 16 Jahren geeignet. Im umgangssprachlichen Kontext der heutigen Jugendkultur kann der Inhalt dieses Artikels als heftig oder direkt empfunden werden. Wenn du weiterlesen möchtest, klicke bitte auf den untenstehenden Link. Wir übernehmen keine Haftung für eventuelle Unannehmlichkeiten oder Schäden, die durch den Inhalt des folgenden Artikels entstehen könnten. Dieser Beitrag ist eine Reaktion auf die Frustration vieler Menschen, die die Integrität unserer Demokratie in Frage stellen. Es ist bedauerlich, dass wir als Migranten-Deutsche oft mehr Respekt vor unserem neuen Heimatland zeigen als manche selbsternannte Patrioten. Wenn du mehr über die gesellschaftlichen Gegebenheiten in Deutschland erfahren möchtest, besuche unsere Rubrik \'Was würde Onkel Volkan davon halten?\'. <a href="'.get_permalink($post->ID).'">Klicken Sie hier, um weiterzulesen.</a></p>';
+    if (has_category('was-wuerde-onkel-volkan-davon-halten', $post->ID) && is_single()) {
+        // Überprüfen, ob die Warnung bereits gesehen wurde
+        if (!isset($_GET['warnung_gelesen'])) {
+            $content = '<p>Der folgende Beitrag ist möglicherweise nicht für Kinder und Jugendliche unter 16 Jahren geeignet. Im umgangssprachlichen Kontext der heutigen Jugendkultur kann der Inhalt dieses Artikels als heftig oder direkt empfunden werden. Wenn du weiterlesen möchtest, klicke bitte auf den untenstehenden Link. Wir übernehmen keine Haftung für eventuelle Unannehmlichkeiten oder Schäden, die durch den Inhalt des folgenden Artikels entstehen könnten. Dieser Beitrag ist eine Reaktion auf die Frustration vieler Menschen, die die Integrität unserer Demokratie in Frage stellen. Es ist bedauerlich, dass wir als Migranten-Deutsche oft mehr Respekt vor unserem neuen Heimatland zeigen als manche selbsternannte Patrioten. Wenn du mehr über die gesellschaftlichen Gegebenheiten in Deutschland erfahren möchtest, besuche unsere Rubrik \'Was würde Onkel Volkan davon halten?\'. <a href="'.get_permalink($post->ID).'?warnung_gelesen=1">Klicken Sie hier, um weiterzulesen.</a></p>';
+        }
     }
     return $content;
 }
 
 add_filter('the_content', 'content_warning_filter');
+
 
